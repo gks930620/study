dbfbe928dad248c00b3c8fc5cec9c36b65511963
gks930620 <gks930620@naver.com>
2023-12-27 10:47:10 +0900

    https://effortguy.tistory.com/238

--- src/com/study/Main2.java
+++ src/com/study/Main2.java
@@ -4,10 +4,6 @@ import com.study.common.vo.PagingVO;
 
 public class Main2 {
     public static void main(String[] args) {
-
-        String searchType="T";
-        System.out.println('A'=='B');
-        System.out.println('A'==65);
     }
 
 
